f9fdfb833d4f5a7110b3c5e60920dc79faf17916
keaunsol Na <knsol1992@naver.com>
2024-10-05 19:31:31 +0900

    Main.java 불필요 코드 제거

--- src/main/java/org/example/javapractice/Main.java
+++ src/main/java/org/example/javapractice/Main.java
@@ -1,37 +1,10 @@
 package org.example.javapractice;
 
 import org.example.javapractice.JFrame.MainFrame;
-import org.example.javapractice.dto.Espresso.TallIcedCaffeAmericano;
-
-import java.util.Arrays;
 
 public class Main {
 
-    private static Class<?> chosenMenu;
-    private static Object chosenObject;
-
     public static void main (String[] args) {
-
-        MainFrame mainFrame = new MainFrame();
-        try
-        {
-            chosenMenu =  Class.forName("GrandeJavaChipFrappuccino");
-            chosenObject = chosenMenu.newInstance();
-
-        }
-        catch (InstantiationException e)
-        {
-            e.printStackTrace();
-        }
-        catch (IllegalAccessException e)
-        {
-            e.printStackTrace();
-        }
-        catch (ClassNotFoundException e)
-        {
-            e.printStackTrace();
-        }
-
-
+        new MainFrame();
     }
 }
