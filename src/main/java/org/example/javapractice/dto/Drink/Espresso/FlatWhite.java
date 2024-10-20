943179ef00a87d2c426f8857629b7b402c01da20
[gyparkdev] <[gyparkdev@gmail.com]>
2024-10-20 23:09:21 +0900

    인터페이스 추가, 메뉴 추가

--- src/main/java/org/example/javapractice/dto/Drink/Espresso/FlatWhite.java
+++ src/main/java/org/example/javapractice/dto/Drink/Espresso/FlatWhite.java
@@ -1,9 +1,11 @@
 package org.example.javapractice.dto.Drink.Espresso;
 
+import org.example.javapractice.Annotation.Espresso;
 import org.example.javapractice.Interface.GetInformation;
 
 import java.util.Map;
 
+@Espresso
 public class FlatWhite implements GetInformation {
 
     public FlatWhite () {
